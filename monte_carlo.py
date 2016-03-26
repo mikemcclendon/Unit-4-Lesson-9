46f40b4ed1e31c274d156c7b4f43e6e937bf2f22
Mike McClendon <mike.mcclendon@gmail.com>
2016-03-26 17:26:17 -0400

    second

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,16 +1,14 @@
-'''This script demonstrates simulations of coin flipping'''
+
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.random import normal
 
-# creating a six sided die, was not sure what the question was asking given that the 
-# results of the coin flip were normally distributed
+# creating a dice that has normally distributed sides
 
 class Dice(object):
-    mu, sigma = 0, 0.1
-    sides = (1 , 2, 3, 4, 5, 6)
-    last_result = None
-
+    sides = normal(size=1000)
+    
     def roll(self):
         self.last_result = result = random.choice(self.sides)
         return result
@@ -18,6 +16,8 @@ class Dice(object):
 def create_dice(number):
     return [Dice() for _ in xrange(number)]
 
+maxlist = []
+minlist = []
 coinlist = []
 
 #rolling a thousand dice a thousand times and counting how frequently 1 hits
@@ -25,13 +25,29 @@ dice = create_dice(1000)
 for i in range(1000):
 	dielist = []
 	for die in dice:
+		coinlist.append(die.roll())	
 		dielist.append(die.roll())	
-	coinlist.append(dielist.count(1))
-
+	maxlist.append(max(dielist))
+	minlist.append(min(dielist))
 	
 #plotting histogram, looks somewhat normal 
 hist, bin_edges = np.histogram(coinlist, density=True)
 plt.bar(bin_edges[:-1], hist, width = .3)
 plt.xlim(min(bin_edges), max(bin_edges))
+plt.title('total results')
+plt.show() 
+	
+#plotting histogram max, very skewed right
+histm, bin_edgesm = np.histogram(maxlist, density=True)
+plt.bar(bin_edgesm[:-1], histm, width = .3)
+plt.xlim(min(bin_edgesm), max(bin_edgesm))
+plt.title('max')
+plt.show() 
+
+#plotting histogram min, very skewed left
+histmin, bin_edgesmin = np.histogram(minlist, density=True)
+plt.bar(bin_edgesmin[:-1], histmin, width = .3)
+plt.xlim(min(bin_edgesmin), max(bin_edgesmin))
+plt.title('min')
 plt.show() 
 
